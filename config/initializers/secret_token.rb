ea6d047eaa34501b53fac62346ea03aa01024adc
mazy learns <tepidtenacity@gmail.com>
2012-03-19 19:45:06 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reddit::Application.config.secret_token = '787bec82188c5683e6c7b852bb941365ae93bf70f71032be10798f96b0d064fbfdb989315713b54bc50f30cba1c474dfe37e72857c11496533e7fe296ee24eb7'
